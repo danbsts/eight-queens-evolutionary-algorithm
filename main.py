c8f814d5b0a952562c76047d1841604823e3f04b
rodrigoap2 <rap2@cin.ufpe.br>
2021-02-27 15:34:09 -0300

    Define part1 functions

---
+++ main.py
@@ -0,0 +1,47 @@
+# Representação
+
+def calcFitness(population):
+    for element in population:
+        fenotype = genotypeIntoFenotype(population)
+    #Calculo do fitness, retorna [(genotipo, fitness),...]
+
+def genotypeIntoFenotype(genotype):
+    # 000000000000000000000000 -> [0,0,0,0,0,0.0,0]
+
+    return fenotype
+
+def cutAndCrossfill(parent1, parent2):
+
+    return child
+
+def mutate(child):
+
+    return mutatedChild
+
+def parentSelection(parents):
+
+    return selectedParents #2 parents
+
+def survivalSelection(population):
+    #Return survivors
+    return newPopulation
+
+def initPopulation(populationSize):
+    #Creates a random population
+    return population
+
+def eval(populationFitness):
+
+    return solution
+
+def main():
+    population = initPopulation(100)
+    populationFitness = calcFitness(population)
+    solution = eval(populationFitness)
+    count = 0
+    while(solution != None and count < 10000){
+        
+        count += 1
+    }
+
+    print(solution)
