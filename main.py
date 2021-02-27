f2ccf995b5a10f605430e52d8e082f4e48083e39
rodrigoap2 <rap2@cin.ufpe.br>
2021-02-27 15:58:08 -0300

    Implement genotypeIntoFenotype function

--- main.py
+++ main.py
@@ -1,47 +1,54 @@
 # Representação
 
 def calcFitness(population):
-    for element in population:
-        fenotype = genotypeIntoFenotype(population)
+   for element in population:
+       fenotype = genotypeIntoFenotype(population)
     #Calculo do fitness, retorna [(genotipo, fitness),...]
 
 def genotypeIntoFenotype(genotype):
     # 000000000000000000000000 -> [0,0,0,0,0,0.0,0]
+    actual = 0
+    fenotype = []
+    for i in range(3, len(genotype)+3, 3):
+        fenotype.append(int(genotype[(actual*3):i],2))
+        actual += 1
 
     return fenotype
 
 def cutAndCrossfill(parent1, parent2):
 
-    return child
+   return child
 
 def mutate(child):
 
-    return mutatedChild
+   return mutatedChild
 
 def parentSelection(parents):
 
-    return selectedParents #2 parents
+   return selectedParents #2 parents
 
 def survivalSelection(population):
     #Return survivors
-    return newPopulation
+   return newPopulation
 
 def initPopulation(populationSize):
     #Creates a random population
-    return population
+   return population
 
 def eval(populationFitness):
 
-    return solution
+   return solution
 
 def main():
-    population = initPopulation(100)
-    populationFitness = calcFitness(population)
-    solution = eval(populationFitness)
-    count = 0
-    while(solution != None and count < 10000){
-        
-        count += 1
-    }
+   population = initPopulation(100)
+   populationFitness = calcFitness(population)
+   solution = eval(populationFitness)
+   count = 0
+   while(solution != None and count < 10000){
+       
+       count += 1
+   }
 
-    print(solution)
+   print(solution)
+
+main()
