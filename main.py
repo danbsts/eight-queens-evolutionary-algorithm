d2d680c606d83fdac089fd1d00d500cb907f4277
Daniel <dmrb2@cin.ufpe.br>
2021-02-27 17:49:20 -0300

    Rename variables

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# Representação
 import random
 
-def calcFitness(population):
+def calculate_fitness(population):
     result = []
-    fenotype_list = list(map(genotypeIntoFenotype, population))
+    fenotype_list = list(map(genotype_to_fenotype, population))
     for fenotype in fenotype_list:
         penalty = 1
         for idx, element in enumerate(fenotype):
@@ -12,8 +11,7 @@ def calcFitness(population):
         result.append((fenotype, 1/penalty))
     return result
 
-def genotypeIntoFenotype(genotype):
-    # 000000000000000000000000 -> [0,0,0,0,0,0.0,0]
+def genotype_to_fenotype(genotype):
     actual = 0
     fenotype = []
     for i in range(3, len(genotype)+3, 3):
@@ -23,36 +21,36 @@ def genotypeIntoFenotype(genotype):
     return fenotype
 
 
-def fenotypeIntoGenotype(fenotype):
-    def addZeros(element):
+def fenotype_to_genotype(fenotype):
+    def add_zeros(element):
         missing = len(element)
         return (3-missing)*'0' + element
     
-    return ''.join(map(addZeros, map(lambda x: "{0:b}".format(x), fenotype)))
+    return ''.join(map(add_zeros, map(lambda x: "{0:b}".format(x), fenotype)))
 
-def crossfill(child, parent,cutPoint):
-    index = cutPoint
+def crossfill(child, parent,cut_point):
+    index = cut_point
     while(len(child) < 8):
         if not(parent[index] in child):
             child.append(parent[index])
         index= (index + 1)%8
     return child
 
-def cutAndCrossfill(parent1, parent2):
-    fenoParent1 = genotypeIntoFenotype(parent1)
-    fenoParent2 = genotypeIntoFenotype(parent2)
-    print(fenoParent1)
-    print(fenoParent2)
-    cutPoint = random.randint(0,6)
+def cut_and_crossfill(parent1, parent2):
+    fenotype_parent1 = genotype_to_fenotype(parent1)
+    fenotype_parent2 = genotype_to_fenotype(parent2)
+    print(fenotype_parent1)
+    print(fenotype_parent2)
+    cut_point = random.randint(0,6)
 
-    child1 = fenoParent1[0:cutPoint]
-    child2 = fenoParent2[0:cutPoint]
-    child1 = crossfill(child1,fenoParent2,cutPoint)
-    child2 = crossfill(child2,fenoParent1,cutPoint)
+    child1 = fenotype_parent1[0:cut_point]
+    child2 = fenotype_parent2[0:cut_point]
+    child1 = crossfill(child1,fenotype_parent2,cut_point)
+    child2 = crossfill(child2,fenotype_parent1,cut_point)
     print(child1)
     print(child2)
-    child1 = fenotypeIntoGenotype(child1)
-    child2 = fenotypeIntoGenotype(child2)
+    child1 = fenotype_to_genotype(child1)
+    child2 = fenotype_to_genotype(child2)
 
     return [child1,child2]
 
@@ -68,61 +66,45 @@ def mutate(child):
 def parent_selection(population):
     parents = select_random_parents(population)
     parents.sort(key=lambda tup: tup[1], reverse=True)
-    selectedParents = parents[0:2]
-    return selectedParents #2 parents
+    selected_parents = parents[0:2]
+    return selected_parents #2 parents
 
 def select_random_parents(population):
-    randomParents = []
+    random_parents = []
     for i in range(5):
-        randomParents.append(population[random.randint(0,100)])
-    return randomParents
+        random_parents.append(population[random.randint(0,100)])
+    return random_parents
     
-def survivalSelection(population):
+def survival_selection(population):
     population.sort(key=lambda tup: -tup[1])
     return population[:-2]
 
-def initPopulation(populationSize):
-    #Creates a random population
+def init_population(population_size):
     population = []
-    while populationSize > 0:
-        population.append(generateChild())
-        populationSize -= 1
+    while population_size > 0:
+        population.append(generate_child())
+        population_size -= 1
     return population
 
-def generateChild():
-    child = ""
-    count = 0
-    while count < 24:
-        randomNumber = random.random()
-        if randomNumber <= 0.5:
-            child = child + "0"
-        else:
-            child = child + "1"
-        count += 1
-    return child
+def generate_child():
+    child = [0,1,2,3,4,5,6,7]
+    random.shuffle(child)
+    return fenotype_to_genotype(child)
 
-def eval(populationFitness):
-    for individual in populationFitness:
+def eval(population_fitness):
+    for individual in population_fitness:
         if individual[1] == 1:
             return individual[0]
     
     return None
 
 def main():
-    # parent1 = [0,2,4,1,5,3,6,7]
-    # parent2 = [7,6,5,4,3,2,1,0]
-    # parent1 = fenotypeIntoGenotype(parent1)
-    # parent2 = fenotypeIntoGenotype(parent2)
-    # cutAndCrossfill(parent1,parent2)
-    population = initPopulation(100)
-    print(population)
-    populationFitness = calcFitness(population)
-    solution = eval(populationFitness)
+    population = init_population(100)
+    population_fitness = calculate_fitness(population)
+    solution = eval(population_fitness)
     count = 0
-    while solution != None and count < 10000:
+    while solution == None and count < 2:
       count += 1
-    
-
     print(solution)
 
 main()
